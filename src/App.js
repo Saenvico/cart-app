2a7125f7efbd73f21322c3116cd1feec442b2c9c
Salavija <theaura8@gmail.com>
2023-01-18 11:45:03 +0200

    add external database

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -6,6 +7,29 @@ import Products from './components/Shop/Products';
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      const response = await fetch(
+        'https://test-ac9ba-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed');
+      }
+
+      const responseData = await response.json();
+    };
+
+    //PUT req overides existing data - in this case existing cart with incoming data (usual is POST)
+    //cart os put as dependency so it would be executed each time cart changes
+    //logic - firstly update redux, then take updated data and send to database
+  }, [cart]);
 
   return (
     <Layout>
