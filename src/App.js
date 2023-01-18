e180b231b502b071d33317d8b1b185e7103ff9f1
Salavija <theaura8@gmail.com>
2023-01-18 11:54:05 +0200

    add notifications

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { uiActions } from './store/ui-slice';
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!',
+        })
+      );
       const response = await fetch(
         'https://test-ac9ba-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
         {
@@ -23,13 +32,29 @@ function App() {
         throw new Error('Sending cart data failed');
       }
 
-      const responseData = await response.json();
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data succesfully',
+        })
+      );
     };
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error',
+          message: 'Sending cart data failed',
+        })
+      );
+    });
 
     //PUT req overides existing data - in this case existing cart with incoming data (usual is POST)
     //cart os put as dependency so it would be executed each time cart changes
     //logic - firstly update redux, then take updated data and send to database
-  }, [cart]);
+  }, [cart, dispatch]);
 
   return (
     <Layout>
