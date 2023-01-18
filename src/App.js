b6d3b9be570187440f2836d984583852aa9f75d5
Salavija <theaura8@gmail.com>
2023-01-18 15:57:44 +0200

    Action creator redux async example

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { uiActions } from './store/ui-slice';
 import Notification from './components/UI/Notification';
+import { sendCartData } from './store/cart-slice';
 
 let isInitial = true;
 
@@ -15,55 +16,66 @@ function App() {
   const dispatch = useDispatch();
   const notification = useSelector((state) => state.ui.notification);
 
-  useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data!',
-        })
-      );
-      const response = await fetch(
-        'https://test-ac9ba-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Sending cart data failed');
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data succesfully',
-        })
-      );
-    };
-
-    if (isInitial) {
+  //ACTION CREATOR
+  useEffect(()=> {
+    if(isInitial){
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error',
-          message: 'Sending cart data failed',
-        })
-      );
-    });
+    dispatch(sendCartData(cart))
+  }, [cart, dispatch])
 
-    //PUT req overides existing data - in this case existing cart with incoming data (usual is POST)
-    //cart os put as dependency so it would be executed each time cart changes
-    //logic - firstly update redux, then take updated data and send to database
-  }, [cart, dispatch]);
+  //POSSIBLE VARIANT OF HOW TO USE ASYNC WITH REDUX
+  // useEffect(() => {
+  //   const sendCartData = async () => {
+  //     dispatch(
+  //       uiActions.showNotification({
+  //         status: 'pending',
+  //         title: 'Sending...',
+  //         message: 'Sending cart data!',
+  //       })
+  //     );
+  //     const response = await fetch(
+  //       'https://test-ac9ba-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
+  //       {
+  //         method: 'PUT',
+  //         body: JSON.stringify(cart),
+  //       }
+  //     );
+
+  //     if (!response.ok) {
+  //       throw new Error('Sending cart data failed');
+  //     }
+
+  //     dispatch(
+  //       uiActions.showNotification({
+  //         status: 'success',
+  //         title: 'Success!',
+  //         message: 'Sent cart data succesfully',
+  //       })
+  //     );
+  //   };
+
+  //   if (isInitial) {
+  //     isInitial = false;
+  //     return;
+  //   }
+
+  //   sendCartData().catch((error) => {
+  //     dispatch(
+  //       uiActions.showNotification({
+  //         status: 'error',
+  //         title: 'Error',
+  //         message: 'Sending cart data failed',
+  //       })
+  //     );
+  //   });
+
+  //   //PUT req overides existing data - in this case existing cart with incoming data (usual is POST)
+  //   //cart os put as dependency so it would be executed each time cart changes
+  //   //logic - firstly update redux, then take updated data and send to database
+  // }, [cart, dispatch]);
 
   return (
     <Fragment>
